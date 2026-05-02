f8b6a2171709bcfe7a53eec193a8050b05b6fa40
tofik819396-cmyk <tofik819396@gmail.com>
2026-05-02 10:26:33 +0530

    fix deploy error

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
 import { useState } from "react";
-import Product from "./Product";
 import { useStore } from "../context/StoreContext";
 import "../styles/Product.css";
 
